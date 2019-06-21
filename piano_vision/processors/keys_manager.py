26262b7cc56a617a1207f4659a4e258a7ef2f8c4
Alex Tompkins <alextompkins3@gmail.com>
2019-06-21 21:52:02 +1200

    Add detection for white keys * Uses Canny edge + Hough lines to detect key boundaries * Then cleans up the result via numerical methods
    Took 1 hour 43 minutes

--- piano_vision/processors/keys_manager.py
+++ piano_vision/processors/keys_manager.py
@@ -7,20 +7,24 @@ class KeysManager:
 	def __init__(self, ref_frame):
 		self.ref_frame = ref_frame
 
-		# Get key contours
+		# Get black key contours
 		thresh = self.threshold(ref_frame)
 		key_contours = self.find_key_contours(thresh)
-		print('{} keys found'.format(len(key_contours)))
 		# cv2.drawContours(ref_frame, key_contours, -1, (255, 0, 255), thickness=2)
 
-		# Get a bounding rectangle for each key
+		# Get a bounding rectangle for each black key
 		self.black_keys = tuple(map(cv2.boundingRect, key_contours))
+		print('{} black keys found'.format(len(self.black_keys)))
 		for rect in self.black_keys:
 			x, y, w, h = rect
-			cv2.rectangle(ref_frame, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+			cv2.rectangle(ref_frame, (x, y), (x + w, y + h), color=(255, 0, 0), thickness=1)
 
-		mean_key_width = np.mean(tuple(map(lambda r: r[2], self.black_keys)))
-		print(mean_key_width)
+		# Get white keys, draw rectangles
+		self.white_keys = tuple(self.find_white_keys(self.ref_frame))
+		print('{} white keys found'.format(len(self.white_keys)))
+		for rect in self.white_keys:
+			x, y, w, h = rect
+			cv2.rectangle(ref_frame, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=1)
 
 		cv2.imshow('ref_frame', ref_frame)
 
@@ -43,3 +47,44 @@ class KeysManager:
 		contours = tuple(filter(lambda c: cv2.contourArea(c) > (mean - 2 * std_dev), contours))
 
 		return contours
+
+	def find_white_keys(self, frame):
+		height = frame.shape[0]
+		cropped = frame[height - round(height / 3.5):height - round(height / 16)].copy()
+		cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+		cropped = cv2.Canny(cropped, 10, 30)
+
+		for row in cropped:
+			for col, val in enumerate(row):
+				if val and col < len(row) - 3 and row[col + 2]:
+					row[col + 1] = val
+					row[col + 2] = 0
+					row[col] = 0
+				if val and col < len(row) - 2 and row[col + 1]:
+					row[col] = 0
+
+		lines = cv2.HoughLinesP(cropped, 1, np.pi / 180, threshold=2, minLineLength=5, maxLineGap=5)
+		boundaries = {0}
+		if lines is not None:
+			for line in lines:
+				for x1, y1, x2, y2 in line:
+					boundaries.add(int(round((x1 + x2) / 2)))
+
+		boundaries.add(cropped.shape[1])
+		boundaries = sorted(boundaries)
+
+		close = []
+		keys = []
+		for i in range(len(boundaries) - 1):
+			start, end = boundaries[i:i+2]
+
+			if abs(start - end) <= 5:
+				close.append(start)
+			else:
+				if close:
+					close.append(start)
+					start = int(np.median(close))
+					close = []
+				keys.append((start, 0, end - start, frame.shape[0]))
+
+		return keys
