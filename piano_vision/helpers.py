ba2173c9b1c6ab7f3edbf4290353f996ef2fbea5
Alex Tompkins <alextompkins3@gmail.com>
2019-06-21 16:16:13 +1200

    Add helpers.py with rotate_image function (for arbitrary number of degrees)
    Took 27 seconds

---
+++ piano_vision/helpers.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+
+def rotate_image(image, angle):
+	image_center = tuple(np.array(image.shape[1::-1]) / 2)
+	rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+	result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+	return result
